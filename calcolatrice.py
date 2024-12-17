5ed023c79931d616f9e3b2ebfefcb34a0963c8f2
ETTORE CERESOLI <ceresoli.ettore.08@itisdalmine.edu.it>
2024-12-17 09:40:01 +0100

    fixed bug

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,10 +1,11 @@
-scelta =-1
-while(scelta!=0):  
+while True:  
     print("bevenuto nella nostra calcolatrice")
     print("inserisci l'operazione che vuoi effettuare")
     scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
 
-    if scelta == 1:
+    if scelta==0:
+        break
+    elif scelta == 1:
         n1= int(input("inserisci il primo numero"))
         n2= int(input("inserisci il secondo numero"))
         print(f"{n1}-{n2}={n1-n2}")
