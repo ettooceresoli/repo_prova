596663b8098e7472301011f98eca0c0266a6d7c2
ETTORE CERESOLI <ceresoli.ettore.08@itisdalmine.edu.it>
2024-12-17 09:33:34 +0100

    implementazione della calcolatrice

---
+++ calcolatrice.py
@@ -0,0 +1,16 @@
+scelta =-1
+while(scelta!=0):  
+    print("bevenuto nella nostra calcolatrice")
+    print("inserisci l'operazione che vuoi effettuare")
+    scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
+
+    if scelta == 1:
+        n1= int(input("inserisci il primo numero"))
+        n2= int(input("inserisci il secondo numero"))
+        print(f"{n1}-{n2}={n1-n2}")
+    elif scelta== 2:
+        n1= int(input("inserisci il primo numero"))
+        n2= int(input("inserisci il secondo numero"))
+        print(f"{n1}+{n2}={n1+n2}")
+    else:
+        print("scelta non corretta!")
