c81abaf41990121a12bd2b4c27d1849ff23c11f2
ettooceresoli <ceresoli.ettore.08@itisdalmine.edu.it>
2024-12-17 09:57:41 +0100

    only function

--- calcolatrice.py
+++ calcolatrice.py
@@ -6,16 +6,3 @@ def sottrazione():
     n1= int(input("inserisci il primo numero"))
     n2= int(input("inserisci il secondo numero"))
     print(f"{n1}-{n2}={n1-n2}")
-while True:  
-    print("bevenuto nella nostra calcolatrice")
-    print("inserisci l'operazione che vuoi effettuare")
-    scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
-
-    if scelta==0:
-        break
-    elif scelta == 1:
-       sottarzione()
-    elif scelta== 2:
-        Somma()
-    else:
-        print("scelta non corretta!")
