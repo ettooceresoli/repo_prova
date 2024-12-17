b11f6e4cc7996bc6a9a325bbcb1f00d7cef2e4e9
ettooceresoli <ceresoli.ettore.08@itisdalmine.edu.it>
2024-12-17 09:56:25 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,13 @@
-print("hello)
+while True:  
+    print("bevenuto nella nostra calcolatrice")
+    print("inserisci l'operazione che vuoi effettuare")
+    scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
+
+    if scelta==0:
+        break
+    elif scelta == 1:
+       sottarzione()
+    elif scelta== 2:
+        Somma()
+    else:
+        print("scelta non corretta!")
